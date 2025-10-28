f60787c8422dc873412f692d2b8d7700bba46437
asanchez365365 <saintxotic123@gmail.com>
2025-10-28 09:33:08 -0500

    Update 2_working_with_strings.py

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -4,8 +4,8 @@
 # ----------------------------------------
 
 # Strings are sequences of characters enclosed in quotes (' ' or " ")
-greeting = "Hello"
-name = "World"
+greeting = "Hello" #string data type
+name = "World" #string data type
 
 # ----------------------------------------
 # Basic String Operations
@@ -20,63 +20,72 @@ print("Concatenated String:", message)  # Output: Hello World
 # ----------------------------------------
 
 phrase = "Python is FUN!"
-
-# Convert all characters to lowercase
+name =" Aaron"
+phrase2= "SUPERCAGEFRAGILISTCIOUS"
+# # Convert all characters to lowercase
 print("Lowercase:", phrase.lower())  # Output: python is fun!
+print("Name Lowercase:", name.lower())
+print("phrase2 Lowercase:", phrase2.lower())
 
-# Convert all characters to uppercase
-print("Uppercase:", phrase.upper())  # Output: PYTHON IS FUN!
-
-# Check if all characters are uppercase
-print("Is Uppercase?", phrase.isupper())  # Output: False
-
-# Find the length of the string
-print("Length of phrase:", len(phrase))  # Output: 14
-
-# ----------------------------------------
-# 3. Indexing and Slicing
-# ----------------------------------------
-
-# Indexing: Access characters by position (0-based index)
-print("First character:", phrase[0])  # Output: P
-print("Last character:", phrase[-1])  # Output: !
-
-# Slicing: Get a range of characters (start inclusive, end exclusive)
-print("Characters 1 to 4:", phrase[1:4])  # Output: yth
-
-# Example combining everything:
-print("Formatted Example:", (greeting + " " + name + "!").upper())
-# Output: HELLO WORLD!
+# # Convert all characters to uppercase
+print("Uppercase:", phrase.upper())
+print("Uppercase:", phrase2.upper())
 
 
-# ----------------------------------------
-# 7. Strings: Advanced Concepts
-# ----------------------------------------
 
-# Creating Strings: use single or double quotes
-greeting1 = 'Hello'
-greeting2 = "Hi there"
 
-# Printing Strings
-print(greeting1)
-print(greeting2)
+# # Check if all characters are uppercase
+print("Is Uppercase?", phrase.isupper())
+print("Is Uppercase",phrase2.isupper())
+# print("Is Uppercase?", phrase.isupper())  # Output: False
 
-# ----------------------------------------
-# String Methods
-# ----------------------------------------
+# # Find the length of the string
+# print("Length of phrase:", len(phrase))  # Output: 14
 
-sentence = "Python is fun to learn"
+# # ----------------------------------------
+# # 3. Indexing and Slicing
+# # ----------------------------------------
 
-# .split(): Splits the string into a list of words
-words = sentence.split()
-print("Split result:", words)
+# # Indexing: Access characters by position (0-based index)
+# print("First character:", phrase[0])  # Output: P
+# print("Last character:", phrase[-1])  # Output: !
 
-# .format(): Allows inserting values into strings using {}
-name = "Marvin"
-age = 35
-intro = "My name is {} and I am {} years old.".format(name, age)
-print(intro)
+# # Slicing: Get a range of characters (start inclusive, end exclusive)
+# print("Characters 1 to 4:", phrase[1:4])  # Output: yth
 
-# You can also use f-strings (Python 3.6+)
-intro_fstring = f"My name is {name} and I am {age} years old."
-print(intro_fstring)
+# # Example combining everything:
+# print("Formatted Example:", (greeting + " " + name + "!").upper())
+# # Output: HELLO WORLD!
+
+
+# # ----------------------------------------
+# # 7. Strings: Advanced Concepts
+# # ----------------------------------------
+
+# # Creating Strings: use single or double quotes
+# greeting1 = 'Hello'
+# greeting2 = "Hi there"
+
+# # Printing Strings
+# print(greeting1)
+# print(greeting2)
+
+# # ----------------------------------------
+# # String Methods
+# # ----------------------------------------
+
+# sentence = "Python is fun to learn"
+
+# # .split(): Splits the string into a list of words
+# words = sentence.split()
+# print("Split result:", words)
+
+# # .format(): Allows inserting values into strings using {}
+# name = "Marvin"
+# age = 35
+# intro = "My name is {} and I am {} years old.".format(name, age)
+# print(intro)
+
+# # You can also use f-strings (Python 3.6+)
+# intro_fstring = f"My name is {name} and I am {age} years old."
+# print(intro_fstring)
