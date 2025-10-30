60a994b99a3150a6e516f007907c3e16dc793ca7
asanchez365365 <saintxotic123@gmail.com>
2025-10-30 13:55:37 +0000

    declaration

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -42,6 +42,14 @@ print("Is Uppercase",phrase2.isupper())
 # # Find the length of the string
 # print("Length of phrase:", len(phrase))  # Output: 14
 
+declaration_of_independence = "IN CONGRESS, JULY 4, 1776 The unanimous Declaration of the thirteen united States of America 
+WHEN in the Course of human events, it becomes necessary for one people to dissolve the
+political bands which have connected them with
+another, and to assume among the powers of the
+earth, the separate and equal station to which
+the Laws of Nature and of Nature’s God entitle
+them, a decent respect to the opinions of mankind requires that they should declare the
+causes which impel them to the separation" 
 # # ----------------------------------------
 # # 3. Indexing and Slicing
 # # ----------------------------------------
