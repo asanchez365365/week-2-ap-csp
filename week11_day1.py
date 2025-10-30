af4d8c95edbda47c23f087a0674a721f5b203a6c
asanchez365365 <saintxotic123@gmail.com>
2025-10-30 13:49:58 +0000

    more work

--- week11_day1.py
+++ week11_day1.py
@@ -27,3 +27,15 @@ num3= int(input ("what is your 3rd fav")) #5
 num4= int(input ("what is your 4th fav")) #4
 print(num1 + num2) #output 6
 print(num3 + num4) #output 54
+
+#  Step 4: User Input Practice
+# Ask the user two questions and combine answers
+
+
+first_answer = input("What is your favorite color? ")
+second_answer = input("What is your favorite animal? ")
+
+
+
+# ⚙️ Step 5: Final Challenge (combine it all)
+# Use math and strings together
