e94dddf87611813440e5084d2d66c382d8ff67fb
asanchez365365 <saintxotic123@gmail.com>
2025-10-30 14:07:23 +0000

    working on strings

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -46,6 +46,12 @@ declaration_of_independence = "The unanimous Declaration of the thirteen united 
 # # 3. Indexing and Slicing
 # # ----------------------------------------
 
+chicago_mayor = "Johnson"
+
+print(chicago_mayor[0])
+print(chicago_mayor[-1])
+print(chicago_mayor[4])
+
 # # Indexing: Access characters by position (0-based index)
 # print("First character:", phrase[0])  # Output: P
 # print("Last character:", phrase[-1])  # Output: !
