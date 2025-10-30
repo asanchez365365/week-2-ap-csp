915c37b79c22a771d046dcb6323d7759f6cdbfe9
asanchez365365 <saintxotic123@gmail.com>
2025-10-30 14:32:05 +0000

    advanced strings

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -100,12 +100,13 @@ print(phrase3[-1])
 # # String Methods
 # # ----------------------------------------
 
-# sentence = "Python is fun to learn"
+sentence = "Python is fun to learn"
 
 # # .split(): Splits the string into a list of words
-# words = sentence.split()
-# print("Split result:", words)
-
+words = sentence.split()
+print("Split result:", words)
+words2=sentence.join("")
+print(words2)
 # # .format(): Allows inserting values into strings using {}
 # name = "Marvin"
 # age = 35
